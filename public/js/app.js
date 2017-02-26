11cc1bbec8127e4a3fbcf9f2b47fe55d4f263438
Marco Cheng <mkpc@uw.edu>
2017-02-25 20:24:40 -0800

    Bring data back to front-end when loaded

--- public/js/app.js
+++ public/js/app.js
@@ -1,145 +1,87 @@
 angular.module('MyApp', ['ngMaterial', 'ngMessages'])
-    .controller('AppCtrl', function ($scope, $mdDialog) {
+    .controller('AppCtrl', AppCtrl)
+    .controller('DialogController', DialogController);
 
-        $scope.todos = [
-            {
-                what: 'Buy apple juice',
-                who: 'Min Li Chan',
-                when: '11/23/2016',
-                notes: " I'll be in your neighborhood doing errands"
-            },
-            {
-                what: 'Fix Ryan computer',
-                who: 'Min Li Chan',
-                when: '2/14/2016',
-                notes: " I'll be in your neighborhood doing errands"
-            },
-            {
-                what: 'Brunch with Raymond',
-                who: 'Min Li Chan',
-                when: '9/30/2016',
-                notes: " I'll be in your neighborhood doing errands"
-            },
-            {
-                what: 'Oil change',
-                who: 'Min Li Chan',
-                when: '12/23/2016',
-                notes: " I'll be in your neighborhood doing errands"
-            },
-            {
-                what: 'Camping!',
-                who: 'Min Li Chan',
-                when: '12/24/2016',
-                notes: " I'll be in your neighborhood doing errands"
-            },
-            {
-                what: 'Buy apple juice',
-                who: 'Min Li Chan',
-                when: '11/23/2016',
-                notes: " I'll be in your neighborhood doing errands"
-            },
-            {
-                what: 'Fix Ryan computer',
-                who: 'Min Li Chan',
-                when: '2/14/2016',
-                notes: " I'll be in your neighborhood doing errands"
-            },
-            {
-                what: 'Brunch with Raymond',
-                who: 'Min Li Chan',
-                when: '9/30/2016',
-                notes: " I'll be in your neighborhood doing errands"
-            },
-            {
-                what: 'Oil change',
-                who: 'Min Li Chan',
-                when: '12/23/2016',
-                notes: " I'll be in your neighborhood doing errands"
-            },
-            {
-                what: 'Camping!',
-                who: 'Min Li Chan',
-                when: '12/24/2016',
-                notes: " I'll be in your neighborhood doing errands"
-            },
-            {
-                what: 'Buy apple juice',
-                who: 'Min Li Chan',
-                when: '11/23/2016',
-                notes: " I'll be in your neighborhood doing errands"
-            },
-            {
-                what: 'Fix Ryan computer',
-                who: 'Min Li Chan',
-                when: '2/14/2016',
-                notes: " I'll be in your neighborhood doing errands"
-            },
-            {
-                what: 'Brunch with Raymond',
-                who: 'Min Li Chan',
-                when: '9/30/2016',
-                notes: " I'll be in your neighborhood doing errands"
-            },
-            {
-                what: 'Oil change',
-                who: 'Min Li Chan',
-                when: '12/23/2016',
-                notes: " I'll be in your neighborhood doing errands"
-            },
-            {
-                what: 'Camping!',
-                who: 'Min Li Chan',
-                when: '12/24/2016',
-                notes: " I'll be in your neighborhood doing errands"
+function AppCtrl($scope, $mdDialog, $http) {
+    $http.get('/book')
+        .then(function (response) {
+            console.log(response.data);
+            $scope.todos = response.data;
+        });
+
+    $scope.save = function () {
+        if (!$scope.newTodo || $scope.newTodo.length < 1) return;
+        var todo = new Todos({name: $scope.newTodo, completed: false});
+
+        todo.$save(function () {
+            $scope.todos.push(todo);
+            $scope.newTodo = ''; // clear textbox
+        });
+    };
+
+    $scope.cancel = function (index) {
+        $scope.todos[index] = angular.copy($scope.editing[index]);
+        $scope.editing[index] = false;
+    };
+
+    $scope.remove = function (index) {
+        var todo = $scope.todos[index];
+        Todos.remove({id: todo._id}, function () {
+            $scope.todos.splice(index, 1);
+        });
+    };
+
+    $scope.selectTask = function (ev, index) {
+        $scope.editing = angular.copy($scope.todos[index]);
+        console.log($scope.editing);
+
+        $mdDialog.show({
+            controller: DialogController,
+            controllerAs: 'DialogController',
+            templateUrl: 'dia.temp.html',
+            parent: angular.element(document.body),
+            targetEvent: ev,
+            clickOutsideToClose: true,
+            locals: {
+                task: $scope.todos[index]
             }
-        ];
-
-        $scope.doSecondaryAction = function (event) {
-            $mdDialog.show(
-                $mdDialog.alert()
-                    .title('Secondary Action')
-                    .textContent('Secondary actions can be used for one click actions')
-                    .ariaLabel('Secondary click demo')
-                    .ok('Neat!')
-                    .targetEvent(event)
-            );
-        };
-
-        $scope.showAdvanced = function (ev, index) {
-            $mdDialog.show({
-                controller: function DialogController($scope, $mdDialog, user) {
-                    //make ajax call to get task
-                   // console.log(user);
-                    $scope.user = user;
-                    $scope.hide = function () {
-                        $mdDialog.hide();
-                    };
-
-                    $scope.delete = function () {
-                        $mdDialog.cancel();
-                    };
-
-                    $scope.answer = function (answer) {
-                        console.log(answer);
-                        $mdDialog.hide(answer);
-                    };
-                },
-                controllerAs: 'ctrl',
-                templateUrl: 'dia.temp.html',
-                parent: angular.element(document.body),
-                targetEvent: ev,
-                clickOutsideToClose: true,
-                fullscreen: $scope.customFullscreen, // Only for -xs, -sm breakpoints.
-                locals: {
-                    user: $scope.todos[index]
-                }
-            })
-                .then(function (answer) {
+        })
+            .then(
+                function (answer) {
                     $scope.status = 'You said the information was "' + answer + '".';
                 }, function () {
                     $scope.status = 'You cancelled the dialog.';
                 });
+    };
+}
 
-        };
-    });
+
+function DialogController($scope, $mdDialog, task) {
+    $scope.task = task;
+
+    (function parseDate() {
+        if ($scope.task.updated !== null) {
+            $scope.task.updated = new Date($scope.task.updated);
+        }
+        if ($scope.task.hasOwnProperty('due')) {
+            $scope.task.due = new Date($scope.task.due);
+            console.log($scope.task.due);
+        }
+    })();
+
+    $scope.hide = function () {
+        $mdDialog.hide();
+    };
+
+    $scope.delete = function () {
+        $mdDialog.cancel();
+    };
+
+    $scope.done = function (task) {
+        console.log('done',task);
+        console.log($scope.editing);
+        $mdDialog.hide(task);
+    };
+}
+
 
